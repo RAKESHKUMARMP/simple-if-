624400acd9efa8b84f21d74eb2c974bfa8e62332
RAKESHKUMARMP <159103741+RAKESHKUMARMP@users.noreply.github.com>
2024-04-01 21:47:06 +0530

    Add files via upload

---
+++ task.java
@@ -0,0 +1,10 @@
+public class task {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Rakesh Kumar");
+    }
+}
+
+
+// simple if
+// print hello rakesh kumar
